3a84a7184d38791a31f755ec2fbaeff6c05776fb
HookDev-arch <ip.docenko_v.a@bk.ru>
2026-01-13 18:12:27 +0500

    Update ipregion.sh

--- ipregion.sh
+++ ipregion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPT_URL="https://github.com/Davoyan/ipregion"
+SCRIPT_URL="https://github.com/HookDev-arch/ipregion/"
 DEPENDENCIES=("jq" "curl" "util-linux")
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
 SPINNER_SERVICE_FILE=$(mktemp "${TMPDIR:-/tmp}/ipregion_spinner_XXXXXX")
